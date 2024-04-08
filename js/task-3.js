050874355d6e89ec286bd281c57d9228e511c543
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-04-08 15:07:25 +0300

    /

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector("#name-input")
-const nameOutputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", handelInput)
-
-function handelInput(event) {
-  const newName = event.target.value;
-  nameOutputEl.textContent = newName.trim() ? newName : "Anonymous";
-}
